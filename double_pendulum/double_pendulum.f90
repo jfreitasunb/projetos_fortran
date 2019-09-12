74e6caa1e2c71f0367440404139fe24d5412dce4
José <Freitas>
2019-09-12 15:15:03 -0300

    Lendo os dados iniciais.

--- double_pendulum/double_pendulum.f90
+++ double_pendulum/double_pendulum.f90
@@ -3,6 +3,13 @@ program double_pendulum
 
     integer, parameter :: extra  = selected_real_kind(p=24,r=1000)
 
-    ! real (kind = extra)
+    real (kind = extra) :: x1, y1, L1, theta1, m1, x2, y2, L2, theta2, m2 !Posicao, comprimento, angulo e massa dos pêndulos 
+
+    !-----------Leitura dos dados iniciais
+    print*, '#Entre com o comprimento, angulo e massa dos pendulos: L1 theta1 m1 L2 theta2 m2: '
+
+    read*, L1, theta1, m1, L2, theta2, m2
+
+
     
 end program double_pendulum
